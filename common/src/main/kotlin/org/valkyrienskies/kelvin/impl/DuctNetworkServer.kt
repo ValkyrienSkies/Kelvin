a467fa1153e5b53e484c1495959636c1e0d63375
PriestOfFerns <timofeydenin@gmail.com>
2025-03-02 16:05:27 +0300

    logging

--- common/src/main/kotlin/org/valkyrienskies/kelvin/impl/DuctNetworkServer.kt
+++ common/src/main/kotlin/org/valkyrienskies/kelvin/impl/DuctNetworkServer.kt
@@ -591,18 +591,20 @@ class DuctNetworkServer(
 
         for (gas in gasMasses) gasMoles[gas.key] = (gas.value/gas.key.density)/22.4
 
-        var reaction_moles = Double.MAX_VALUE
+        var reactionMoles = Double.MAX_VALUE
+
+        if (inputGasses.size == 0) return KELVINLOGGER.error("empty inputGasses in gas reaction.")
 
         for (gas in inputGasses) {
             if (gas.key !in gasMoles || gasMoles[gas.key]!! < 0.001) return
 
             val thisOutput =  gasMoles[gas.key]!! / gas.value
-            if (thisOutput < reaction_moles) reaction_moles = thisOutput
+            if (thisOutput < reactionMoles) reactionMoles = thisOutput
         }
 
-        for (gas in inputGasses) modGasMass(ductNodePos,gas.key,-reaction_moles * gas.value * gas.key.density * 22.4)
+        for (gas in inputGasses) modGasMass(ductNodePos,gas.key,-reactionMoles * gas.value * gas.key.density * 22.4)
 
-        for (gas in outputGasses) modGasMass(ductNodePos,gas.key,reaction_moles * gas.value * gas.key.density * 22.4)
+        for (gas in outputGasses) modGasMass(ductNodePos,gas.key,reactionMoles * gas.value * gas.key.density * 22.4)
 
     }
 
