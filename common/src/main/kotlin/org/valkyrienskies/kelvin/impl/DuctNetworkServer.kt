8699ab4ce20935d445fe425d6b6bbeaedcd908e8
PriestOfFerns <timofeydenin@gmail.com>
2025-04-10 13:01:44 +0300

    Add Gibbs free energy change to gas reactions

--- common/src/main/kotlin/org/valkyrienskies/kelvin/impl/DuctNetworkServer.kt
+++ common/src/main/kotlin/org/valkyrienskies/kelvin/impl/DuctNetworkServer.kt
@@ -575,34 +575,35 @@ class DuctNetworkServer(
 
             for (reaction in reactions.values) {
                 var con = false
-                reaction.requirements.forEach {if (!it.key.apply_requirement(level, node, this, it.value)) { con = true
-                    return@forEach
-                }}
+                reaction.requirements.forEach {if (!it.key.apply_requirement(level, node, this, it.value)) { con = true; return@forEach }}
                 if (con) continue
 
 
-                calcReaction(node, gasMasses, reaction.gasses, reaction.result)
+                calcReaction(node, gasMasses, reaction.gasses, reaction.result, reaction.energy)
             }
         }
     }
 
-    private fun calcReaction(ductNodePos: DuctNodePos, gasMasses: HashMap<GasType, Double>, inputGasses: HashMap<GasType, Int>, outputGasses: HashMap<GasType, Int>) {
+    private fun calcReaction(ductNodePos: DuctNodePos, gasMasses: HashMap<GasType, Double>, inputGasses: HashMap<GasType, Int>, outputGasses: HashMap<GasType, Int>, deltaEnergy: Double) {
         val gasMoles = HashMap<GasType, Double>()
 
         for (gas in gasMasses) gasMoles[gas.key] = (gas.value/gas.key.density)/22.4
 
-        var reaction_moles = Double.MAX_VALUE
+        var reactionMoles = Double.MAX_VALUE
+
 
         for (gas in inputGasses) {
             if (gas.key !in gasMoles || gasMoles[gas.key]!! < 0.001) return
 
             val thisOutput =  gasMoles[gas.key]!! / gas.value
-            if (thisOutput < reaction_moles) reaction_moles = thisOutput
+            if (thisOutput < reactionMoles) reactionMoles = thisOutput
         }
 
-        for (gas in inputGasses) modGasMass(ductNodePos,gas.key,-reaction_moles * gas.value * gas.key.density * 22.4)
+        for (gas in inputGasses) modGasMass(ductNodePos,gas.key,-reactionMoles * gas.value * gas.key.density * 22.4)
 
-        for (gas in outputGasses) modGasMass(ductNodePos,gas.key,reaction_moles * gas.value * gas.key.density * 22.4)
+        for (gas in outputGasses) modGasMass(ductNodePos,gas.key,reactionMoles * gas.value * gas.key.density * 22.4)
+
+        modHeatEnergy(ductNodePos, deltaEnergy * reactionMoles)
 
     }
 
--- common/src/main/kotlin/org/valkyrienskies/kelvin/impl/KelvinReactionDataLoader.kt
+++ common/src/main/kotlin/org/valkyrienskies/kelvin/impl/KelvinReactionDataLoader.kt
@@ -22,7 +22,6 @@ object KelvinReactionDataLoader {
             resourceManager: ResourceManager,
             profiler: ProfilerFiller
         ) {
-            println("Applying Data loader")
             gas_reactions.clear()
             objects.forEach { (location, element) ->
                 try {
@@ -76,9 +75,12 @@ object KelvinReactionDataLoader {
                 result[gasType] = gasParts
             }
 
-            val parsed_reaction = GasReaction(gasses, requirements, result)
-            println(parsed_reaction)
-            gas_reactions[origin] = parsed_reaction
+
+            val energy = if (jObject.has("energy")) jObject["energy"].asDouble else 0.0
+
+            val parsedReaction = GasReaction(gasses = gasses, requirements = requirements, energy = energy, result = result)
+            println(parsedReaction)
+            gas_reactions[origin] = parsedReaction
 
         }
     }
