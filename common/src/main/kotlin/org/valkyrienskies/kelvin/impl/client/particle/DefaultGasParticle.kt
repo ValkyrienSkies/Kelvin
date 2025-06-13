a699d9023b13c91b575dec9204437b68ed2dae56
PriestOfFerns <timofeydenin@gmail.com>
2025-06-13 23:30:27 +0300

    make DefaultGasParticle not have randomized speed

--- common/src/main/kotlin/org/valkyrienskies/kelvin/impl/client/particle/DefaultGasParticle.kt
+++ common/src/main/kotlin/org/valkyrienskies/kelvin/impl/client/particle/DefaultGasParticle.kt
@@ -7,6 +7,13 @@ import net.minecraft.core.particles.SimpleParticleType
 
 class DefaultGasParticle(level: ClientLevel, x: Double, y: Double, z: Double, xSpeed: Double, ySpeed: Double, zSpeed: Double): TextureSheetParticle(level, x, y, z, xSpeed, ySpeed, zSpeed) {
 
+    init {
+        this.gravity = 0f
+        this.xd = xSpeed
+        this.yd = ySpeed
+        this.zd = zSpeed
+    }
+
     override fun getRenderType(): ParticleRenderType {
         return ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT
     }
