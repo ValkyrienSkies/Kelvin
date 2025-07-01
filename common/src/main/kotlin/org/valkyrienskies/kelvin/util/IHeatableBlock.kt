66ce9c02dd27bb4bdc0760b80818286b04f3c257
ThePlasticPotato <theplasticpotato@gmail.com>
2025-07-01 09:52:54 -0400

    in the wrong damn interface

--- common/src/main/kotlin/org/valkyrienskies/kelvin/util/IHeatableBlock.kt
+++ common/src/main/kotlin/org/valkyrienskies/kelvin/util/IHeatableBlock.kt
@@ -8,16 +8,6 @@ import org.valkyrienskies.kelvin.api.DuctNodePos
 import org.valkyrienskies.kelvin.serialization.NodeNBTUtil
 
 interface IHeatableBlock {
-    fun saveData(tag: CompoundTag, pos: DuctNodePos) {
-        NodeNBTUtil.serializeNode(pos, KelvinMod.getKelvinByPlatform()!!, tag)
-    }
-    fun loadData(tag: CompoundTag, pos: DuctNodePos) {
-        val nodeData = tag.getCompound("kelvin_node_data")
-        if (nodeData.isEmpty) {
-            return
-        }
-        NodeNBTUtil.deserializeNode(pos, KelvinMod.getKelvinByPlatform()!!, tag)
-    }
 
     companion object {
         val GAS_HEAT_LEVEL: EnumProperty<GasHeatLevel> = EnumProperty.create("gas_heat_level", GasHeatLevel::class.java)
