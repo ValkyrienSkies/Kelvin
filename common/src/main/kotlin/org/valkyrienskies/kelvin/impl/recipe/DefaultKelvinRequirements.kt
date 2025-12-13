882dcdc238e1688bd88a319c1e1e3dbcfce3fb19
PriestOfFerns <timofeydenin@gmail.com>
2025-12-13 21:41:19 +0300

    add `inhibited_by` reaction requirement

--- common/src/main/kotlin/org/valkyrienskies/kelvin/impl/recipe/DefaultKelvinRequirements.kt
+++ common/src/main/kotlin/org/valkyrienskies/kelvin/impl/recipe/DefaultKelvinRequirements.kt
@@ -2,14 +2,16 @@ package org.valkyrienskies.kelvin.impl.recipe
 
 import com.google.gson.JsonElement
 import net.minecraft.network.chat.Component
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.level.Level
 import org.valkyrienskies.kelvin.KelvinMod
 import org.valkyrienskies.kelvin.api.DuctNetwork
 import org.valkyrienskies.kelvin.api.DuctNodePos
 import org.valkyrienskies.kelvin.api.recipe.GasReactionRequirement
+import org.valkyrienskies.kelvin.impl.registry.GasTypeRegistry
 
 object DefaultKelvinRequirements {
-    val defaultRequirements = listOf(minTemperature, maxTemperature, minPressure, maxPressure)
+    val defaultRequirements = listOf(minTemperature, maxTemperature, minPressure, maxPressure, inhibitedBy)
 
     object minTemperature: GasReactionRequirement(KelvinMod.asResouceLocation("min_temperature")) {
         override fun apply_requirement(level: Level, ductNode: DuctNodePos, network: DuctNetwork<*>, value: JsonElement): Boolean {
@@ -69,6 +71,26 @@ object DefaultKelvinRequirements {
             val doubleValue = value.asDouble
 
             return Component.literal("Maximum Pressure: $doubleValue Pa")
+        }
+    }
+
+    object inhibitedBy: GasReactionRequirement(KelvinMod.asResouceLocation("inhibited_by")) {
+        override fun apply_requirement(level: Level, ductNode: DuctNodePos, network: DuctNetwork<*>, value: JsonElement): Boolean {
+            val gasTypeId = value.asJsonObject["gas"].asString
+            val gasType = GasTypeRegistry.getGasType(ResourceLocation.of(gasTypeId, ':'))
+            val ratio = value.asJsonObject["ratio"].asDouble
+
+            val gasMasses = network.getGasMassAt(ductNode)
+            val mass = gasMasses[gasType] ?: 0.0
+
+            return mass / gasMasses.values.sum() <= ratio
+        }
+
+        override fun get_text(value: JsonElement): Component {
+            val gasType = value.asJsonObject["gas"].asString
+            val ratio = value.asJsonObject["ratio"].asDouble
+
+            return Component.literal("Inhibitied by: $gasType with ${ratio*100}%")
         }
     }
 }
