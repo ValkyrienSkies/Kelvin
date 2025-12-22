bdab37dce721a52395fad9d2de401d1c01b46bd1
PriestOfFerns <timofeydenin@gmail.com>
2025-12-22 19:14:24 +0300

    Revert "make kelvin particle registry register to correct modid"
    This reverts commit 3f1c4d3cfa40056659c45d5c02d1413522d150c2.

--- common/src/main/kotlin/org/valkyrienskies/kelvin/KelvinParticles.kt
+++ common/src/main/kotlin/org/valkyrienskies/kelvin/KelvinParticles.kt
@@ -3,8 +3,6 @@ package org.valkyrienskies.kelvin
 import dev.architectury.registry.client.particle.ParticleProviderRegistry
 import dev.architectury.registry.registries.DeferredRegister
 import dev.architectury.registry.registries.RegistrySupplier
-import net.minecraft.client.particle.Particle
-import net.minecraft.core.particles.ParticleType
 import net.minecraft.core.registries.Registries
 import org.valkyrienskies.kelvin.impl.client.particle.DefaultGasParticle.DefaultGasParticleType
 import org.valkyrienskies.kelvin.impl.client.particle.DefaultGasParticleProvider
@@ -14,20 +12,10 @@ import org.valkyrienskies.kelvin.impl.client.particle.DefaultGasParticleProvider
 object KelvinParticles {
 
     val ALL: HashSet<RegistrySupplier<DefaultGasParticleType>> = HashSet()
-    val MODID_TO_PARTICLE_REGISTER = HashMap<String, DeferredRegister<ParticleType<*>>>()
+    val PARTICLES = DeferredRegister.create(KelvinMod.MOD_ID, Registries.PARTICLE_TYPE)
 
-
-
-    fun getOrCreateRegistry(modId: String): DeferredRegister<ParticleType<*>> {
-        if (modId !in MODID_TO_PARTICLE_REGISTER)
-            MODID_TO_PARTICLE_REGISTER[modId] = DeferredRegister.create(KelvinMod.MOD_ID, Registries.PARTICLE_TYPE)
-
-
-        return MODID_TO_PARTICLE_REGISTER[modId]!!
-    }
-
-    fun registerDefaultParticle(modid: String, name: String):  RegistrySupplier<DefaultGasParticleType> {
-        val supplier = getOrCreateRegistry(modid).register(name) { DefaultGasParticleType() }
+    fun registerDefaultParticle(name: String):  RegistrySupplier<DefaultGasParticleType> {
+        val supplier = PARTICLES.register(name) { DefaultGasParticleType() }
         ALL.add(supplier)
         KelvinMod.KELVINLOGGER.info("Registered particle: ${supplier.id}")
         return supplier
@@ -35,7 +23,7 @@ object KelvinParticles {
 
     fun init() {
         KelvinMod.KELVINLOGGER.info("Registering Kelvin default gas particles...")
-        MODID_TO_PARTICLE_REGISTER.forEach { it.value.register() }
+        PARTICLES.register()
 
     }
 
