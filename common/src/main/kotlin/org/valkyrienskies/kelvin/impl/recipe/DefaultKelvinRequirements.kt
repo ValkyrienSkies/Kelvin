b9068b52a87cce38c075f913ee671cad17f3a836
PriestOfFern <priestoffern@gmail.com>
2026-02-17 14:39:00 +0300

    minor rendering tweaks

--- common/src/main/kotlin/org/valkyrienskies/kelvin/impl/recipe/DefaultKelvinRequirements.kt
+++ common/src/main/kotlin/org/valkyrienskies/kelvin/impl/recipe/DefaultKelvinRequirements.kt
@@ -90,7 +90,7 @@ object DefaultKelvinRequirements {
             val gasType = value.asJsonObject["gas"].asString
             val ratio = value.asJsonObject["ratio"].asDouble
 
-            return Component.literal("Inhibitied by: $gasType with ${ratio*100}%")
+            return Component.literal("Inhibitied by: $gasType(${ratio*100}%)")
         }
     }
 }
