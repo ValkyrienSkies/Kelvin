891d21e8c61c4f4fa49be5c85f48a71f55520bf6
PriestOfFern <timofeydenin@gmail.com>
2026-07-10 22:20:40 +0300

    even more jitter fixes

--- common/src/test/kotlin/org/valkyrienskies/kelvin/FlowConservationTest.kt
+++ common/src/test/kotlin/org/valkyrienskies/kelvin/FlowConservationTest.kt
@@ -32,23 +32,32 @@ import org.valkyrienskies.kelvin.impl.solvers.JacobiSolver
 class FlowConservationTest : KelvinTestBase() {
 
     @Test
-    fun `ignore dense near-equilibrium pressure jitter`() {
-        val a = DuctNodePos(0.0, 0.0, 0.0)
-        val b = DuctNodePos(1.0, 0.0, 0.0)
-        network.solver = JacobiSeidelSolver() // only implemented fix for jacobi (TODO: do it for the other solvers too)
-        network.addNode(a, defaultPipe(a))
-        network.addNode(b, defaultPipe(b))
-        network.addEdge(a, b, defaultPipeEdge(a, b))
-        network.addGasAtTemperature(a, TEST_AIR, 5.0, 300.0)
-        network.addGasAtTemperature(b, TEST_AIR, 5.0, 300.0)
+    fun `ignore dense equilibrated loop circulation`() {
+        val engine = DuctNodePos(0.0, 0.0, 0.0)
+        val ductA = DuctNodePos(1.0, 0.0, 0.0)
+        val ductB = DuctNodePos(0.0, 1.0, 0.0)
+        network.solver = JacobiSeidelSolver()
+        network.addNode(engine, defaultPipe(engine))
+        network.addNode(ductA, defaultPipe(ductA))
+        network.addNode(ductB, defaultPipe(ductB))
+        network.addEdge(engine, ductA, defaultPipeEdge(engine, ductA))
+        network.addEdge(engine, ductB, defaultPipeEdge(engine, ductB))
+        network.addEdge(ductA, ductB, defaultPipeEdge(ductA, ductB))
 
-        network.modHeatEnergy(a, network.getHeatEnergy(a) * 5e-6)
+        network.addGasAtTemperature(engine, TEST_AIR, 6.0, 300.0)
+        network.addGasAtTemperature(ductA, TEST_AIR, 5.0, 300.0)
+        network.addGasAtTemperature(ductB, TEST_AIR, 5.0, 300.0)
 
-        simulate(steps = 1)
+        simulate(steps = 400)
 
-        val flow = network.getEdgeBetween(a, b)!!.currentFlowRate
-        assertTrue(flow == 0.0) {
-            "Solver should not report flow for sub-tolerance dense equilibrium jitter, got $flow kg/s"
+        val flows = listOf(
+            network.getEdgeBetween(engine, ductA)!!.currentFlowRate,
+            network.getEdgeBetween(engine, ductB)!!.currentFlowRate,
+            network.getEdgeBetween(ductA, ductB)!!.currentFlowRate,
+        )
+        val maxFlow = flows.maxOf { kotlin.math.abs(it) }
+        assertTrue(maxFlow < 1e-6) {
+            "Solver should not report circulating flow in a dense equilibrated loop, got $flows kg/s"
         }
     }
 
